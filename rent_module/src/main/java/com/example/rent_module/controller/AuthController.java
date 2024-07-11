58e3ea484817212b8797452da70fe09f5df88c5d
PetrJava <persiapedro@gmail.com>
2024-07-11 19:55:54 +0300

    first

--- rent_module/src/main/java/com/example/rent_module/controller/AuthController.java
+++ rent_module/src/main/java/com/example/rent_module/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.example.rent_module.controller;
 
-import com.example.rent_module.module.entity.UserInfoEntity;
 import static com.example.rent_module.controller.PathConstants.REGISTRATION;
+import com.example.rent_module.module.entity.UserInfoEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +10,6 @@ public class AuthController {
 
     @PostMapping(REGISTRATION)
     public String registrationUser(UserInfoEntity userInfoEntity) {
-
+        return null;
     }
 }
